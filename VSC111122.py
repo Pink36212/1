7cde2aeffcdf044d64bd3cbccc3c23a7ecb87027
Pink36212 <112546213+Pink36212@users.noreply.github.com>
2022-11-11 15:24:12 +0700

    Add files via upload

--- VSC111122.py
+++ VSC111122.py
@@ -1,3 +1,4 @@
-number1 = input('Enter number1')
-number2 = input('Enter number2')
+number1 = float(input('Enter number1'))
+number2 = float(input('Enter number2'))
 product = number1*number2
+print(number1,"x",number2,"=",product)
